f549d9d92b040f5c2dabedc9807ad7089b2820aa
Lizzie <elizabethmawer@hotmail.com>
2017-05-13 19:45:20 +0100

    added jQuery commands for pie chart

--- js/lizzie.js
+++ js/lizzie.js
@@ -5,22 +5,51 @@
 
 $(document).ready(function(){
     
+    //navigation dropdown
    $("#navbutton").click(function(){
        $("nav").slideToggle();
    })
     
-
+   //if wrong answer selected on bar chart
     $(".wrong").click(function(){
         $("#answer").text("No that is incorrect. Only 20% of tech startups, across the world, are founded by women");  
     })
     
+    //if correct answer selected on bar chart
     $("#correct").click(function(){
         $("#answer").text("Yes that is correct.  Only 20% of tech startups, across the world, are founded by women");
     })
     
+    //animation for bar chart
     $(".chart").click(function(){
         $("#correct").addClass("animated flash");
     })
+    
+    //if correct answer selected on pie chart
+    $(".segmentOne").click(function(){
+        $("#answer").text("Yes that is correct. Only 5% of leadership positions, in the tech industry, are held by women.")
+    })
+    
+    //animation for pie chart
+    $(".segmentOne").click(function(){
+        $(".segmentOne").addClass("animated slideOutUp");
+    })
+    
+    //if wrong answer selected on pie chart
+    $(".segmentTwo").click(function(){
+        $("#answer").text("No that is incorrect. Only 5% of leadership positions, in the tech industry, are held by women.")
+    })
+    
+    //if wrong answer selected on pie chart
+    $(".segmentThree").click(function(){
+        $("#answer").text("No that is incorrect. Only 5% of leadership positions, in the tech industry, are held by women.")
+    })
+    
+    //if wrong answer selected on pie chart
+    $(".segmentFour").click(function(){
+        $("#answer").text("No that is incorrect. Only 5% of leadership positions, in the tech industry, are held by women.")
+    })
+    
         
 });
 
