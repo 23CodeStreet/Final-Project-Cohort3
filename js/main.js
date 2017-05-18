dbe6a730bd354019ab07c6b32e257259f9697d8c
A Vidmar <anja_vidmar@yahoo.co.uk>
2017-05-18 18:09:55 +0100

    anjas header and css fix

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
-$(document).ready(function(){
-   $(".container").click(function(){
-       $("nav").slideToggle();
-   });
+$(document).ready(function () {
+	
+//Navigation slide	
+	$(".menu-container").click(function () {
+		$("nav").slideToggle();
+	});
+
+//ANIMATED BURGER MENU
+	function myFunction(x) {
+		x.classList.toggle("change");
+	}
+
+//Button press shadow
+	$("#start-button").mousedown(function () {
+		$(this).css("background-color", "#999");
+	});
+	$("#start-button").mouseup(function () {
+		$(this).css("background-color", "#ccc");
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 });
-function myFunction(x) {
-    x.classList.toggle("change");
-}
