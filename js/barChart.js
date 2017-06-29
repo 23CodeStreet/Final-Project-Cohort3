5f8ee29378534c705ef3b28fc92c7b184ad57d81
Lizzie <elizabethmawer@hotmail.com>
2017-06-29 20:29:54 +0100

    answer field now populates with yes or no text after submit button pressed on bar chart

--- js/barChart.js
+++ js/barChart.js
@@ -7,26 +7,29 @@ $(document).ready(function(){
   
 //**********BAR CHART***********//
 
-   //if correct answer selected on bar chart
-    
-$("#correct" && "button").click(function(){
-            $("#answer").text("Yes that is correct.  Only 20% of tech startups, across the world, are founded by women");
-            })   
+//if wrong answer selected on bar chart
 
-//if wrong answer selected on bar chart                            
-$(".wrong" && "button").click(function(){
-            $("#answer").text("No that is incorrect. Only 20% of tech startups, across the world, are founded by women");
-            })
-
-//animation for bar chart & next question text
-
+$(".wrong").click(function(){
     $("button").click(function(){
+        $("#answer").text("No that is incorrect. Only 20% of tech startups, across the world, are founded by women");
         $("span").text("Next Question")
-        $("#submitTwo").attr("href", "Q3_simplePieChart.html")
         $("#correct").addClass("animated flash")
         $("button").click(function(){
  window.location.replace('Q3_simplePieChart.html')
         })
     })
-    
+})
+
+
+//if corrct answer selected on bar chart
+$("#correct").click(function(){
+    $("button").click(function(){
+        $("#answer").text("Yes that is correct.  Only 20% of tech startups, across the world, are founded by women");
+        $("span").text("Next Question")
+        $("#correct").addClass("animated flash")
+        $("button").click(function(){
+ window.location.replace('Q3_simplePieChart.html')
+        })
+      })
+    })
 });
