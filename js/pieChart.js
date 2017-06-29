bd1d7ee5acb8fb9967bf507165ce2b47ec0d9186
Lizzie <elizabethmawer@hotmail.com>
2017-06-29 20:22:12 +0100

    answer field now populates with true and false text after submit button pressed

--- js/pieChart.js
+++ js/pieChart.js
@@ -7,28 +7,34 @@ $(document).ready(function(){
     
     //**********PIE CHART***********//
     
-//if wrong answer selected on pie chart
-
-$(".wrongCircle" && "button").click(function(){
-        $("#answer").text("No that is incorrect. Only 5% of leadership positions, in the tech industry, are held by women.")
-    })
-    
 //if correct answer selected on pie chart
 
-$(".correctCircle" && "button").click(function(){
+$(".correctCircle").click(function(){
+  $("button").click(function(){
         $("#answer").text("Yes that is correct. Only 5% of leadership positions, in the tech industry, are held by women.")
-    })
-    
- //animation for pie chart & next question text
-
-$("button").click(function(){
         $("span").text("Next Question")
         $(".semiCircle2").addClass("animated flash")
+        
+        $("button").click(function(){
+  window.location.replace('Q4_pieChart.html')
+        })
+      })
+    })
+    
+//if incorrect answer is selected on pie chart
+
+$(".wrongCircle").click(function() {
+  
+$("button").click(function(){
+        $("#answer").text("No that is incorrect. Only 5% of leadership positions, in the tech industry, are held by women.")
+        $("span").text("Next Question")
+        $(".semiCircle2").addClass("animated flash")
+        
           $("button").click(function(){
   window.location.replace('Q4_pieChart.html')
+        })
+      })  
     })
-})
-   
 });
     
     
