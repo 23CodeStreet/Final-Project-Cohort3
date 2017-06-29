2e39ea84173a63e99ad5baa54f204d9ac624cb2c
Lizzie <elizabethmawer@hotmail.com>
2017-06-29 20:12:23 +0100

    answer field now populates with true and false text after submit button pressed

--- js/trueFalse.js
+++ js/trueFalse.js
@@ -1,28 +1,37 @@
-$(document).ready(function(){
-  
-  //navigation dropdown
-   $("#navbutton").click(function(){
-       $("nav").slideToggle();
-   })
+$(document).ready(function() {
 
-//**********TRUE OR FALSE***********//
-    
-    //if True is selected
-    $("").click(function(){
-        $("#answer").text("Yes that is correct. In the 8 largest tech companies, female tech positions are growing 238% faster than male positions!");
-    })
-    
-    //if False is selected
-    $("").click(function(){
-        $("#answer").text("No that is incorrect. In the 8 largest tech companies, female tech positions are growing 238% faster than male positions!");
-    })
-    
-    //animation for True or False page
-    $("button").click(function(){
-        $("span").text("Further Information")
-        $("#female").addClass("animated bounce")
-        $("button").click(function(){
+      //navigation dropdown
+      $("#navbutton").click(function() {
+          $("nav").slideToggle();
+      })
+
+      //**********TRUE OR FALSE***********//
+
+      //if False is selected
+      $("input.false").click(function() {
+
+          $("button").click(function() {
+                $("#answer").text("No that is incorrect. In the 8 largest tech companies, female tech positions are growing 238% faster than male positions!")
+                $("span").text("Further Information")
+              $("#female").addClass("animated bounce")
+
+            $("button").click(function() {
             window.location.replace('further-information.html')
+            })
         })
+    })
+
+        //if True is selected
+
+        $("input.true").click(function() {
+
+        $("button").click(function() {
+              $("span").text("Further Information")
+              $("#female").addClass("animated bounce")
+              $("#answer").text("Yes that is correct. In the 8 largest tech companies, female tech positions are growing 238% faster than male positions!")
+              $("button").click(function() {
+          window.location.replace('further-information.html')
+          })
+      })
     })
 });
