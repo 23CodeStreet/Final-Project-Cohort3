3cc467c807708c562ca28ad392c6baa2faea0483
Lizzie <elizabethmawer@hotmail.com>
2017-06-30 15:07:42 +0100

    answer field populates after submit button pressed - four possible outcomes, animation works after submit button pressed, submit button text changes to next question and holds href value.

--- js/techCompanies.js
+++ js/techCompanies.js
@@ -7,34 +7,69 @@ $(document).ready(function(){
    
    //**********TECH COMPANIES***********//
     
-    //if correct tech company is selected
-    $("").click(function(){
-        $("#answer").text("Yes that is correct. In 2015 Ebay was named as a more diverse company.")
-    })
-    
-    //if incorrect tech company is selected
-    $("").click(function(){
-        $("#answer").text("No that is incorrect. In 2015 Ebay was named as a more diverse company.")
-    })
-    
-     //if correct % of staff is selected
-    $("").click(function(){
-        $("#answer").text("Yes that is correct. 24% of Ebay's staff were women.")
-    })
-    
-    //if incorrect % of staff is selected
-    $("").click(function(){
-        $("#answer").text("No that is incorrect. 24% of Ebay's staff were women.")
-    })
-    
-    //animation for tech companies & next question text
-
-    $("button").click(function(){
+  //if correct tech company & % is selected
+   
+   $("#ebay").click(function(){
+     $("#twentyFour").click(function(){
+      $("button").click(function(){
+        $("#answer").text("Yes that is correct. In 2015 Ebay was named as a more diverse company. 24% of Ebay's staff were women.")
         $("span").text("Next Question")
         $("#ebay").addClass("animated flip")
         $("#twentyFour").addClass("animated flip");
           $("button").click(function(){
   window.location.replace('Q2_barChart.html')
+          })
         })
+      })
+    })
+
+  //if incorrect tech company & % is selected
+
+    $(".company").click(function(){
+     $(".staff").click(function(){
+      $("button").click(function(){
+        $("#answer").text("No that is incorrect. In 2015 Ebay was named as a more diverse company. 24% of Ebay's staff were women.")
+        $("span").text("Next Question")
+        $("#ebay").addClass("animated flip")
+        $("#twentyFour").addClass("animated flip");
+          $("button").click(function(){
+  window.location.replace('Q2_barChart.html')
+          })
+        })
+      })
+    })
+
+    
+//if incorrect tech company but correct % is selected
+
+    $(".company").click(function(){
+     $("#twentyFour").click(function(){
+      $("button").click(function(){
+        $("#answer").text("The company you selected is incorrect. In 2015 Ebay was named as a more diverse company. However, you are correct that 24% of Ebay's staff were women.")
+        $("span").text("Next Question")
+        $("#ebay").addClass("animated flip")
+        $("#twentyFour").addClass("animated flip");
+          $("button").click(function(){
+  window.location.replace('Q2_barChart.html')
+          })
+        })
+      })
+    })
+
+
+//if correct tech company but incorrect % is selected
+
+    $("#ebay").click(function(){
+     $(".staff").click(function(){
+      $("button").click(function(){
+        $("#answer").text("The company you selected is correct. In 2015 Ebay was named as a more diverse company. However, the percentage you selected is incorrect. 24% of Ebay's staff were women.")
+        $("span").text("Next Question")
+        $("#ebay").addClass("animated flip")
+        $("#twentyFour").addClass("animated flip");
+          $("button").click(function(){
+  window.location.replace('Q2_barChart.html')
+          })
+        })
+      })
     })
 });
