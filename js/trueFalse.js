21d82d2d15f3f5f91878d9f22e150d94a4951992
Lizzie <elizabethmawer@hotmail.com>
2017-07-04 21:21:33 +0100

    making user experience better on mobile

--- js/trueFalse.js
+++ js/trueFalse.js
@@ -13,7 +13,8 @@ $(document).ready(function() {
           $("button").click(function() {
                 $("#answer").text("No that is incorrect. In the 8 largest tech companies, female tech positions are growing 238% faster than male positions!")
                 $("span").text("Further Information")
-              $("#female").addClass("animated bounce")
+                
+            $("#female").addClass("animated bounce")
 
             $("button").click(function() {
             window.location.replace('further-information.html')
